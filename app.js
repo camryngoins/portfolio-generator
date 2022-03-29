681fcb04fdad82e4cd8e72539437b186f6dc5a5c
camryngoins <camryngoins00@gmail.com>
2022-03-29 15:33:04 -0500

    created profile data function

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log(document);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+printProfileData(profileDataArgs);
